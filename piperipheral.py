18feed56669f2a5956f1bace40fa8f96d2e9b55a
Simon Aubury <response_github@vsimon.com>
2020-09-16 19:39:27 +1000

    More swiches

--- piperipheral.py
+++ piperipheral.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-from gpiozero import Button
 from signal import pause
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO 
 
 NULL_CHAR = chr(0)
 MOD_NONE = NULL_CHAR
+MOD_SHFT = chr(32)
+KEY_NONE = 0
 
-gpio_button_map = dict([(5, (4,MOD_NONE)), (6, (5, MOD_NONE)), (27, (6, MOD_NONE))])
+gpio_button_map = dict([
+    (5, (4,MOD_NONE, KEY_NONE,MOD_NONE))    # Button 1
+  , (6, (5, MOD_NONE, 6,MOD_NONE))          # Switch 1
+  , (27, (7, MOD_SHFT, KEY_NONE,MOD_NONE))  # Button 2
+  ])
 
 
 def write_report(report):
@@ -18,19 +23,22 @@ def keyboard_release():
     # Release keys
     write_report(NULL_CHAR*8)
 
-def keyboard_do(letter):
-    write_report(NULL_CHAR*2+chr(letter)+NULL_CHAR*5)
+def keyboard_do(letter, mod):
+    write_report(mod + NULL_CHAR + chr(letter) + NULL_CHAR*5)
     keyboard_release()
 
-def button_callback(channel):
-    print('Channel {}'.format(channel))  
-    key_id, mod_id = gpio_button_map[channel][0], gpio_button_map[channel][1]
-    print('Key:{} Mod:{}'.format(key_id, mod_id))
+def gpio_callback(channel):
     if GPIO.input(channel):     
-        print('Button UP {}'.format(channel))  
+        # print('Button-RELEASE / Switch:DOWN {}'.format(channel))  
+        key_id, mod_id = gpio_button_map[channel][2], gpio_button_map[channel][3]
     else:                 
-        print('Button DOWN {}'.format(channel))  
-        keyboard_do(key_id)
+        # print('Button-PRESS / Switch:UP {}'.format(channel))  
+        key_id, mod_id = gpio_button_map[channel][0], gpio_button_map[channel][1]
+
+    if (key_id != KEY_NONE):
+        # print('Channel {}'.format(channel))  
+        print('Key:{} Mod:{}'.format(key_id, mod_id))
+        keyboard_do(key_id, mod_id)
         
 
 
@@ -41,7 +49,7 @@ def main_key_loop():
     for k, v in gpio_button_map.items():
         print('Setup GPIO {}'.format(k))
         GPIO.setup(k, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
-        GPIO.add_event_detect(k ,GPIO.BOTH,callback=button_callback, bouncetime=200) 
+        GPIO.add_event_detect(k ,GPIO.BOTH,callback=gpio_callback, bouncetime=200) 
     pause()
 
 if __name__ == '__main__':
