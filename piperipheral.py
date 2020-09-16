a22269a6a7a81c1f713967ef16196ef9f3426bc9
Simon Aubury <response_github@vsimon.com>
2020-09-16 19:56:53 +1000

    More swiches

--- piperipheral.py
+++ piperipheral.py
@@ -5,13 +5,20 @@ import RPi.GPIO as GPIO
 
 NULL_CHAR = chr(0)
 MOD_NONE = NULL_CHAR
-MOD_SHFT = chr(32)
+MOD_CNTR = chr(1)
+MOD_SHFT = chr(2)
 KEY_NONE = 0
+
 
 gpio_button_map = dict([
     (5, (4,MOD_NONE, KEY_NONE,MOD_NONE))    # Button 1
   , (6, (5, MOD_NONE, 6,MOD_NONE))          # Switch 1
   , (27, (7, MOD_SHFT, KEY_NONE,MOD_NONE))  # Button 2
+  , (22, (8, MOD_NONE, 9,MOD_NONE))          # Switch 2
+  , (4, (10, MOD_SHFT, KEY_NONE,MOD_NONE))  # Button 3
+  , (17, (11, MOD_NONE, 12,MOD_NONE))          # Switch 3
+  , (2, (6, MOD_CNTR, KEY_NONE,MOD_NONE))  # Button 4
+  , (3, (14, MOD_NONE, 15,MOD_NONE))          # Switch 4
   ])
 
 
@@ -29,15 +36,11 @@ def keyboard_do(letter, mod):
 
 def gpio_callback(channel):
     if GPIO.input(channel):     
-        # print('Button-RELEASE / Switch:DOWN {}'.format(channel))  
         key_id, mod_id = gpio_button_map[channel][2], gpio_button_map[channel][3]
     else:                 
-        # print('Button-PRESS / Switch:UP {}'.format(channel))  
         key_id, mod_id = gpio_button_map[channel][0], gpio_button_map[channel][1]
 
     if (key_id != KEY_NONE):
-        # print('Channel {}'.format(channel))  
-        print('Key:{} Mod:{}'.format(key_id, mod_id))
         keyboard_do(key_id, mod_id)
         
 
@@ -47,7 +50,6 @@ def main_key_loop():
     GPIO.setmode(GPIO.BCM) 
 
     for k, v in gpio_button_map.items():
-        print('Setup GPIO {}'.format(k))
         GPIO.setup(k, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
         GPIO.add_event_detect(k ,GPIO.BOTH,callback=gpio_callback, bouncetime=200) 
     pause()
